701339a490df1c3549b0852459257bdd20def8f4
chuong vo <chuongvo240@gmail.com>
2021-04-25 13:58:06 +0700

    project c#

---
+++ HadoopOnUbuntu/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace HadoopOnUbuntu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.txIntput = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbOutput = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnStart.Location = new System.Drawing.Point(26, 32);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(111, 40);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStatus.Location = new System.Drawing.Point(159, 47);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(81, 25);
+            this.lbStatus.TabIndex = 1;
+            this.lbStatus.Text = "status...";
+            // 
+            // txIntput
+            // 
+            this.txIntput.Location = new System.Drawing.Point(26, 143);
+            this.txIntput.Name = "txIntput";
+            this.txIntput.Size = new System.Drawing.Size(585, 96);
+            this.txIntput.TabIndex = 2;
+            this.txIntput.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Intput:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Output:";
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(26, 298);
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ReadOnly = true;
+            this.tbOutput.Size = new System.Drawing.Size(585, 96);
+            this.tbOutput.TabIndex = 5;
+            this.tbOutput.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 38);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "select file .txt";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 38);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Export file .txt";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(893, 495);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txIntput);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Hadoop 2 Nodes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.RichTextBox txIntput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox tbOutput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ HadoopOnUbuntu/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HadoopOnUbuntu
+{
+    public partial class Form1 : Form
+    {
+        String nameContainer_master= "bold_mccarthy";
+        String nameContainer_slave = "vibrant_wiles";
+        String nameImage_master = "m2:20.04";
+        String nameImage_slave = "slave2:20.04";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void checkStatusContainer(String name)
+        {
+            String cmdText = "docker ps --filter "+"\"name = "+ name + "\"";
+            int result = cmdText.ToString().Split('\n').Length ;
+            if (result == 1)
+            {
+                lbStatus.Text = "Running";
+            }
+            else//0
+            {
+                lbStatus.Text = "Stop";
+            }
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            String cmdText = "docker start bold_mccarthy ";
+            ExeCommand(cmdText);
+            checkStatusContainer("bold_mccarthy");
+        }
+        public string ExeCommand(object cmd)
+        {
+            ProcessStartInfo proc = new ProcessStartInfo("cmd", "/c" + cmd);
+            proc.RedirectStandardOutput = true;
+            proc.RedirectStandardError = true;//bật hiện lỗi cmd
+            proc.UseShellExecute = false;
+            proc.CreateNoWindow = true; //không hiển thị cửa sổ cmd
+            Process p = new Process();
+            p.StartInfo = proc;
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            //in lỗi nếu có
+            string err = p.StandardError.ReadToEnd();
+            tbOutput.Visible = true;
+            tbOutput.Text = err;
+
+            return output;
+        }
+
+    }
+}
